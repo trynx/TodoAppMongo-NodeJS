4716784d76873c03dccc6e2b5a197a6a2d3fadc8
Nico <trynx93@gmail.com>
2018-11-04 15:27:16 +0200

    Add - Test for DELETE /users/signout

--- server/server.js
+++ server/server.js
@@ -147,7 +147,7 @@ app.post('/users/login', (req, res) => {
     });
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
+app.delete('/users/signout', authenticate, (req, res) => {
     req.user.removeToken(req.token).then(() => {
         res.status(200).send();
     }, () => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -302,4 +302,24 @@ describe('POST /users/login', () => {
                 }).catch((err) => done(err));
             });
     });
-})
+});
+
+describe('DELETE /users/signout', () => {
+    it('Should remove auth token on logout', (done) => {
+         // DELETE /users/signout
+         request(app)
+            .delete('/users/signout')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+});
