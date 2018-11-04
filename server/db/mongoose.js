8c28eb412164e5950676810b01f75dc3e59fd7fe
Nico <trynx93@gmail.com>
2018-11-04 17:30:07 +0200

    Update - Config to use JSON file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // mongoose.Promise = global.Promise;
 mongodb://<dbuser>:<dbpassword>@ds145053.mlab.com:45053/todo-app-test
-var url = process.env.MONGODB_URI || 'mongodb://techupp:upptech1@ds145923.mlab.com:45923/todo-app-production';
+var url = process.env.MONGODB_URI /*|| 'mongodb://techupp:upptech1@ds145923.mlab.com:45923/todo-app-production'*/;
 mongoose.connect(url, { useNewUrlParser: true });
 
 module.exports = {mongoose};
